e6649fa8989ae6c166b3ae658dcfdec1f97a6e33
jpancotti <jpancotti@icloud.com>
2020-02-06 01:25:14 -0500

    variable steerRatio

--- selfdrive/car/honda/interface.py
+++ selfdrive/car/honda/interface.py
@@ -231,7 +231,8 @@ class CarInterface(CarInterfaceBase):
       ret.mass = 3410. * CV.LB_TO_KG + STD_CARGO_KG
       ret.wheelbase = 2.66
       ret.centerToFront = ret.wheelbase * 0.41
-      ret.steerRatio = 16.0  # 12.3 is spec end-to-end
+      ret.steerRatio = 15.1  # 12.3 is spec end-to-end
+      ret.steerRatioV = 0.008
       tire_stiffness_factor = 0.677
       ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.6], [0.18]]
       ret.longitudinalTuning.kpBP = [0., 5., 35.]
